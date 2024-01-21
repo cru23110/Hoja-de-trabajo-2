4cf91f55594ccd850b13e0773c017c6fb5222016
Jumacruz <jumacruz01@gmail.com>
2024-01-21 15:33:28 -0600

    logic to read txt, and display results

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("datos.txt"));
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                int resultado = PostfixEvaluator.evaluarExpresion(linea);
+                System.out.println("Resultado: " + resultado);
+            }
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
